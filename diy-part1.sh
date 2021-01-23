70eb4d916296a70ec0ccead9d073ff0e8d2a5fec
公子高 <54660876+Gongzigao@users.noreply.github.com>
2021-01-23 11:59:31 +0800

    增加额外插件包

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,29 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# KoolProxyR去广告插件
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+# 微信推送插件
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+# 京东签到插件
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+# adguardhome插件
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+# Clash插件
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+# SmartDNS插件
+git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#强制关机插件
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+#自动关机插件
+git clone https://github.com/sirpdboy/luci-app-autopoweroff package/luci-app-autopoweroff
+
+# argon主题
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+# Edge主题
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
