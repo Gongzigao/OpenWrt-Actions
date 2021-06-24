761c688f11bdc22996bbc23f60e1053d2a2f77be
公子高 <54660876+Gongzigao@users.noreply.github.com>
2021-06-24 16:52:36 +0800

    修改内核版本为5.4

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,6 @@
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 # Modify default PassWord
 sed -i 's/root::0:0:99999:7:::/root:$1$ScQIGKsX$q0qEf\/tAQ2wpTR6zIUIjo.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+# 修改内核版本
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.10/KERNEL_TESTING_PATCHVER:=5.4/g' target/linux/x86/Makefile
